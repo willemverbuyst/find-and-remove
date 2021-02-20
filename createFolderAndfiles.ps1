f6b1e4c8eb959d2b1600f47edd60a585735e1e8e
Willem <willemverbuyst@gmail.com>
2021-02-20 06:56:31 +0100

    Refactor code, use variables

--- createFolderAndfiles.ps1
+++ createFolderAndfiles.ps1
@@ -1,16 +1,26 @@
 # Get path
 $Global:currrentLocation = Get-Location
 
-# Clear Profiles folder and its content
-Remove-Item $Global:currrentLocation\Profiles -Force -Recurse
+# Main folder
+$Global:mainFolder = "Profiles"
 
-# Create a new Profiles folder
-mkdir Profiles
+# List with folders to create
+$Global:fileWithfolderNames = "profiles.txt"
 
-# Get the list with folders to create
-$Global:fileWithfolderNames = Get-Content -Path .\profiles.txt
+# Delete Profiles folder and its content
+Remove-Item $Global:currrentLocation\$Global:mainFolder -Force -Recurse
 
-# Loop to create a folder for each item on the list
-foreach ($folder in $Global:fileWithfolderNames) {
-    New-Item -ItemType Directory -Path $Global:currrentLocation\Profiles\$folder
+# # Create a new Profiles folder
+New-Item -ItemType Directory -Path $Global:currrentLocation\$Global:mainFolder
+
+# # Get the names of th folders to create from text-file
+$Global:foldersToCreate = Get-Content -Path $fileWithfolderNames
+
+# # Loop to create a folder for each item on the list
+foreach ($folder in $Global:foldersToCreate) {
+    New-Item -ItemType Directory -Path $Global:currrentLocation\$Global:mainFolder\$folder
 } 
+
+foreach($folder in Get-ChildItem $Global:currrentLocation\Profiles) {
+    New-Item -ItemType File $Global:currrentLocation\$Global:mainFolder\$folder\$fileWithfolderNames
+}
