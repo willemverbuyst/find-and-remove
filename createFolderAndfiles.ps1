7f83a93b94cfed73e54e9ce79ba945940ebfa60d
Willem <willemverbuyst@gmail.com>
2021-02-20 07:10:50 +0100

    Create text files

--- createFolderAndfiles.ps1
+++ createFolderAndfiles.ps1
@@ -10,17 +10,23 @@ $Global:fileWithfolderNames = "profiles.txt"
 # Delete Profiles folder and its content
 Remove-Item $Global:currrentLocation\$Global:mainFolder -Force -Recurse
 
-# # Create a new Profiles folder
+# Create a new Profiles folder
 New-Item -ItemType Directory -Path $Global:currrentLocation\$Global:mainFolder
 
-# # Get the names of th folders to create from text-file
+# Get the names of th folders to create from text-file
 $Global:foldersToCreate = Get-Content -Path $fileWithfolderNames
 
-# # Loop to create a folder for each item on the list
+# Loop to create a folder for each item on the list
 foreach ($folder in $Global:foldersToCreate) {
     New-Item -ItemType Directory -Path $Global:currrentLocation\$Global:mainFolder\$folder
 } 
 
-foreach($folder in Get-ChildItem $Global:currrentLocation\Profiles) {
-    New-Item -ItemType File $Global:currrentLocation\$Global:mainFolder\$folder\$fileWithfolderNames
+# Add 4 txt files to each subfolder
+foreach($folder in Get-ChildItem $Global:currrentLocation\$Global:mainFolder) {
+    for ($i=1; $i -lt 4; $i++) {
+        New-Item -ItemType File $Global:currrentLocation\$Global:mainFolder\$folder\profile$i.txt
+        Add-Content $Global:currrentLocation\$Global:mainFolder\$folder\profile$i.txt "This file should be here."
+    }
+    New-Item -ItemType File $Global:currrentLocation\$Global:mainFolder\$folder\fileToRemove.txt
+    Add-Content $Global:currrentLocation\$Global:mainFolder\$folder\fileToRemove.txt "This file will be deleted."
 }
